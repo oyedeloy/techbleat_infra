c979c54af5cba69aefd85be11e6f93da7880b7dd
dele <oyedeloy@gmail.com>
2020-10-01 23:55:00 -0500

    s3 config removed

--- hello.tf
+++ hello.tf
@@ -3,14 +3,6 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-terraform {
-    backend "s3" {
-        bucket = "Wek_2-del-devops-bucket"
-        key    = "devops_key"
-        region = "us-east-2"
-    }
-}
-
 resource "aws_instance" "first_node" {
   ami                    = "ami-04cd019702b8c27b8"
   instance_type          = "t2.micro"

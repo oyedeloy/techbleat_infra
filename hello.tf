98457fd01f871c7a8d90916ea42bca23e004b163
dele <oyedeloy@gmail.com>
2020-10-01 23:21:34 -0500

    security group error corrected

--- hello.tf
+++ hello.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "first_node" {
   ami                    = "ami-04cd019702b8c27b8"
   instance_type          = "t2.micro"
   key_name               = "devops_key"
-  vpc_security_group_ids = [aws_security_group.demo_2.id]
+  vpc_security_group_ids = [aws_security_group.demo_1.id]
 
   tags = {
     Name        = "Toolbox_1"

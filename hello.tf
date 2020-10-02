1da5030ab247f9c3fb313b40456b768c3034e065
dele <oyedeloy@gmail.com>
2020-10-01 23:23:30 -0500

    second security group error corrected

--- hello.tf
+++ hello.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "second_node" {
   ami                    = "ami-04cd019702b8c27b8"
   instance_type          = "t2.micro"
   key_name               = "devops_key"
-  vpc_security_group_ids = [aws_security_group.demo_2.id]
+  vpc_security_group_ids = [aws_security_group.demo_1.id]
 
   tags = {
     Name        = "Toolbox_2"
